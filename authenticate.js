bf659a3ba6e6c38b2488ef3fa3e20a7aaef3109e
Ivan Bowen <bowenivan16@gmail.com>
2022-12-02 11:21:35 +0300

    configure facebook login route

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/users');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -35,9 +36,34 @@ exports.verifyUser = passport.authenticate('jwt', { session: false }); // cause 
 exports.verifyAdmin = function (req, res, next) {
    if (req.user.admin) {
       next();
- } else {
+   } else {
       err = new Error("You are not authorized to perform this operation!");
       res.statusCode = 403;
       return next(err);
    }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+   clientID: config.facebook.clientId,
+   clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+   User.findOne({ facebookId: profile.id }, (err, user) => {
+      if (err) {
+         return done(err, false);
+      }
+      if (!err && user !== null) {
+         return done(null, user);
+      }
+      else {
+         user = new User({ username: profile.displayName });
+         user.facebookId = profile.id;
+         user.firstname = profile.name.givenName;
+         user.lastname = profile.name.familyName;
+         user.save((err, user) => {
+            if (err) return done(err, null);
+            else return done(null, user);
+         })
+      }
+   });
+}
+));
